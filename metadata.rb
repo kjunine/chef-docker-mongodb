e82acbf89ea3e2eec5479c04d96f6c65fea693cb
Daniel Ku <kjunine@gmail.com>
2014-10-07 16:27:46 +0900

    Adds docker cookbook dependency.

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,6 @@ maintainer_email 'kjunine@gmail.com'
 license          'MIT'
 description      'Installs/Configures docker-mongodb'
 long_description 'Installs/Configures docker-mongodb'
-version          '0.1.0'
+version          '0.2.0'
 
+depends 'docker'
