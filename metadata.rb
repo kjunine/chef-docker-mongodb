6ba2d5708a403a3fcc043e6d2b22c57a1e31c84d
Daniel Ku <kjunine@gmail.com>
2014-10-07 16:15:48 +0900

    Adds initial version.

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'docker-mongodb'
+maintainer       'Daniel Ku'
+maintainer_email 'kjunine@gmail.com'
+license          'MIT'
+description      'Installs/Configures docker-mongodb'
+long_description 'Installs/Configures docker-mongodb'
+version          '0.1.0'
+
